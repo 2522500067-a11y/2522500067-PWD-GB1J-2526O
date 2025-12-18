ca981be7988934a300e6dc71e3a3e771261539f7
2522500067-a11y <2522500067@mahasiswa.atmaluhur.ic.id>
2025-12-18 17:06:40 +0700

    menambahkan kolom aksi di read php

--- pertemuan-12/README.md
+++ pertemuan-12/README.md
@@ -1,1 +1,20 @@
-# pertemuan-12
+# pertemuan-11
+<ol>
+<li>mengupdate fungsi.php/li>
+<li>mengupdate index.php</li>
+<li>menambah update index.php</li>
+<li>menambah index.php</li>
+<li>menambahkan proses.php</li>
+<li>menambahkan lagi proses.php</li>
+<li>menambah proses.php</li>
+<li>menambahkan INSERT dengan prepared statement</li>
+<li>tambahkan SESSION</li>
+<li>simpan pesan sukses/gagal</li>
+<li>menambah kode nomor<li>
+<li>menambah validasi nama dan pesan</li>
+<li>menambah input captcha sederhana</li>
+<li>menambahkan dcreated di tbl tamu</li>
+<li>menambahkan dcreated di read php</li>
+<li>menambahkan localhost yang baru</li>
+<li>menambahkan require di read inc</li>
+<li>menambahkan kolom aksi di read php</li>
---
+++ pertemuan-12/localhost.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 16 Des 2025 pada 11.28
+-- Versi server: 5.7.33
+-- Versi PHP: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pwd2025`
+--
+CREATE DATABASE IF NOT EXISTS `db_pwd2025` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `db_pwd2025`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_tamu`
+--
+
+CREATE TABLE `tbl_tamu` (
+  `cid` int(11) NOT NULL,
+  `cnama` varchar(100) DEFAULT NULL,
+  `cemail` varchar(100) DEFAULT NULL,
+  `cpesan` text,
+  `dcreated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_tamu`
+--
+
+INSERT INTO `tbl_tamu` (`cid`, `cnama`, `cemail`, `cpesan`, `dcreated_at`) VALUES
+(1, 'jelisa dewi komariah', 'jelisa26@gmail.com', 'semangat', '2025-12-16 18:09:14'),
+(2, 'ghina maharani', 'ghina17@gmail.com', 'teman', '2025-12-16 18:09:14'),
+(3, 'lusy monica', 'lusy06@gmail.com', 'happy', '2025-12-16 18:09:14'),
+(4, 'jelisa dewi komariah', 'jelisa26@gmail.com', 'belajar lagi', '2025-12-16 18:09:14'),
+(5, 'Jel', '2522500067@atmaluhur.ic.id', 'semangattt', '2025-12-16 18:09:14'),
+(6, 'GG', 'jelisa26@gmail.com', 'FGH', '2025-12-16 18:09:14'),
+(7, 'jelisa dewi komariah', 'jelisa26@gmail.com', 'qwweerrghh', '2025-12-16 18:09:14'),
+(8, 'jelisa dewi komariah', 'jelisa26@gmail.com', 'dgfhhjhjhj', '2025-12-16 18:09:14'),
+(9, 'Jel', 'jelisa26@gmail.com', 'qwertyyuuu', '2025-12-16 18:09:14'),
+(10, 'Jel', 'jelisa26@gmail.com', 'tqwertyuiu', '2025-12-16 18:09:14');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
